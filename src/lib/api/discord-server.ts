95b4268d4f27ee2c0baaefda6650f4ec6d45b888
joseph-beck <joebeck03@icloud.com>
2023-07-15 13:09:46 +0100

    use cordle-api for guild request

--- src/lib/api/discord-server.ts
+++ src/lib/api/discord-server.ts
@@ -1,21 +1,10 @@
 import type { Server } from "$lib/types/discord";
 
 export async function getServer(id: string): Promise<Server> {
-    let auth = process.env.DISCORD_TOK;
-    if (auth === undefined) throw new Error("DISCORD_TOK is undefined in .env");
-
-    console.log(auth);
-
     const response = await fetch(
-        `https://discord.com/api/guilds/${id}?with_counts=true`,
+        `http://localhost:8080/guild?id=${id}`,
         {
             method: "GET",
-            headers: {
-                "Accept": "*/*",
-                "Content-Type": "application/x-www-form-urlencoded",
-                "Authorization": auth,
-                "User-Agent": "Node/12.14.1"
-            },
         },
     );
 
